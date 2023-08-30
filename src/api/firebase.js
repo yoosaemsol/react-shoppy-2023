5d202d69287706d233b6e951df14d0bdb9474361
yoosaemsol <yoosaemsol@gmail.com>
2023-08-31 01:38:41 +0900

    auth: Integrate API - Login, Logout, and Session Verification with firebase.

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -1,4 +1,11 @@
 import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut,
+  onAuthStateChanged,
+} from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -8,3 +15,24 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
+
+export async function login() {
+  return signInWithPopup(auth, provider)
+    .then((result) => {
+      const user = result.user;
+      return user;
+    })
+    .catch(console.error);
+}
+
+export async function logout() {
+  return signOut(auth).then(() => null);
+}
+
+export async function onUserStateChange(callback) {
+  onAuthStateChanged(auth, (user) => {
+    callback(user);
+  });
+}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { FiShoppingBag } from 'react-icons/fi';
 import { BsFillPencilFill } from 'react-icons/bs';
+import { login, logout, onUserStateChange } from '../api/firebase';
 
 export default function Navbar() {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    onUserStateChange((user) => {
+      setUser(user);
+    });
+  }, []);
+
+  const handleLogin = () => {
+    login().then(setUser);
+  };
+
+  const handleLogout = () => {
+    logout().then(setUser);
+  };
+
   return (
     <header className="flex justify-between p-2 py-8">
       <Link to="/" className="text-3xl font-bold hover:text-brand-active">
@@ -19,9 +35,22 @@ export default function Navbar() {
         <Link to="/products/new" className="text-lg hover:text-brand-active">
           <BsFillPencilFill />
         </Link>
-        <button className="bg-brand py-2 px-6 rounded-full hover:bg-brand-yellow">
-          Login
-        </button>
+        {!user && (
+          <button
+            onClick={handleLogin}
+            className="bg-brand py-2 px-6 rounded-full hover:bg-brand-yellow"
+          >
+            Login
+          </button>
+        )}
+        {user && (
+          <button
+            onClick={handleLogout}
+            className="bg-brand py-2 px-6 rounded-full hover:bg-brand-yellow"
+          >
+            Logout
+          </button>
+        )}
       </nav>
     </header>
   );

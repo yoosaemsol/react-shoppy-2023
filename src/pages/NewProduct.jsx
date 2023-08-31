05b7e73ba64b30040150623992100d9e40a493c5
yoosaemsol <yoosaemsol@gmail.com>
2023-08-31 12:26:06 +0900

    newProducts: Markup for New Products page.

--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -1,5 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from '../components/ui/Button';
 
 export default function NewProduct() {
-  return <div>NewProduct</div>;
+  const [product, setProduct] = useState({});
+  const [file, setFile] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleChange = (e) => {
+    const { name, value, files } = e.target;
+
+    if (name === 'file') {
+      setFile(files && files[0]);
+      return;
+    }
+
+    setProduct((product) => ({ ...product, [name]: value }));
+  };
+
+  return (
+    <section>
+      {file && <img src={URL.createObjectURL(file)} alt="local file" />}
+      <form onsubmit={handleSubmit}>
+        <input
+          type="file"
+          accept="image/*"
+          name="file"
+          required
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="title"
+          value={product.title ?? ''}
+          placeholder="Product name"
+          required
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          name="price"
+          value={product.price ?? ''}
+          placeholder="Price"
+          required
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="category"
+          value={product.category ?? ''}
+          placeholder="Category"
+          required
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="description"
+          value={product.description ?? ''}
+          placeholder="Description"
+          required
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="options"
+          value={product.options ?? ''}
+          placeholder="Options (separated by commas)"
+          required
+          onChange={handleChange}
+        />
+        <Button>Register Product</Button>
+      </form>
+    </section>
+  );
 }
